02fea5bd6891922d76a094b66b4ab8b1aea61748
Luhman-16 <165486185+Luhman-16@users.noreply.github.com>
2024-03-30 10:01:03 -0400

    Add files via upload
    Main game

---
+++ RWArtMonth28/RWArtMonth28/Form1.Designer.cs
@@ -0,0 +1,178 @@
+﻿
+namespace RWArtMonth28
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable nécessaire au concepteur.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Nettoyage des ressources utilisées.
+        /// </summary>
+        /// <param name="disposing">true si les ressources managées doivent être supprimées ; sinon, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Code généré par le Concepteur Windows Form
+
+        /// <summary>
+        /// Méthode requise pour la prise en charge du concepteur - ne modifiez pas
+        /// le contenu de cette méthode avec l'éditeur de code.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox_Bottom = new System.Windows.Forms.GroupBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btn_4 = new System.Windows.Forms.Button();
+            this.btn_3 = new System.Windows.Forms.Button();
+            this.btn_2 = new System.Windows.Forms.Button();
+            this.btn_1 = new System.Windows.Forms.Button();
+            this.pictureBox_Left = new System.Windows.Forms.PictureBox();
+            this.pictureBox_Right = new System.Windows.Forms.PictureBox();
+            this.groupBox_Bottom.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox_Left)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox_Right)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // groupBox_Bottom
+            // 
+            this.groupBox_Bottom.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.groupBox_Bottom.Controls.Add(this.label1);
+            this.groupBox_Bottom.Controls.Add(this.btn_4);
+            this.groupBox_Bottom.Controls.Add(this.btn_3);
+            this.groupBox_Bottom.Controls.Add(this.btn_2);
+            this.groupBox_Bottom.Controls.Add(this.btn_1);
+            this.groupBox_Bottom.Location = new System.Drawing.Point(19, 384);
+            this.groupBox_Bottom.Margin = new System.Windows.Forms.Padding(10);
+            this.groupBox_Bottom.Name = "groupBox_Bottom";
+            this.groupBox_Bottom.Size = new System.Drawing.Size(1386, 368);
+            this.groupBox_Bottom.TabIndex = 0;
+            this.groupBox_Bottom.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.label1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(3, 16);
+            this.label1.Margin = new System.Windows.Forms.Padding(12);
+            this.label1.MinimumSize = new System.Drawing.Size(1200, 200);
+            this.label1.Name = "label1";
+            this.label1.Padding = new System.Windows.Forms.Padding(12);
+            this.label1.Size = new System.Drawing.Size(1380, 230);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Insert Text";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // btn_4
+            // 
+            this.btn_4.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.btn_4.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_4.Location = new System.Drawing.Point(1050, 270);
+            this.btn_4.Margin = new System.Windows.Forms.Padding(12);
+            this.btn_4.MaximumSize = new System.Drawing.Size(321, 83);
+            this.btn_4.Name = "btn_4";
+            this.btn_4.Size = new System.Drawing.Size(321, 83);
+            this.btn_4.TabIndex = 3;
+            this.btn_4.UseVisualStyleBackColor = true;
+            this.btn_4.Click += new System.EventHandler(this.btn_4_Click);
+            // 
+            // btn_3
+            // 
+            this.btn_3.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.btn_3.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_3.Location = new System.Drawing.Point(705, 270);
+            this.btn_3.Margin = new System.Windows.Forms.Padding(12);
+            this.btn_3.MaximumSize = new System.Drawing.Size(321, 83);
+            this.btn_3.Name = "btn_3";
+            this.btn_3.Size = new System.Drawing.Size(321, 83);
+            this.btn_3.TabIndex = 2;
+            this.btn_3.UseVisualStyleBackColor = true;
+            this.btn_3.Click += new System.EventHandler(this.btn_3_Click);
+            // 
+            // btn_2
+            // 
+            this.btn_2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.btn_2.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_2.Location = new System.Drawing.Point(360, 270);
+            this.btn_2.Margin = new System.Windows.Forms.Padding(12);
+            this.btn_2.MaximumSize = new System.Drawing.Size(321, 83);
+            this.btn_2.Name = "btn_2";
+            this.btn_2.Size = new System.Drawing.Size(321, 83);
+            this.btn_2.TabIndex = 1;
+            this.btn_2.UseVisualStyleBackColor = true;
+            this.btn_2.Click += new System.EventHandler(this.btn_2_Click);
+            // 
+            // btn_1
+            // 
+            this.btn_1.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_1.Location = new System.Drawing.Point(15, 270);
+            this.btn_1.Margin = new System.Windows.Forms.Padding(12);
+            this.btn_1.MaximumSize = new System.Drawing.Size(321, 83);
+            this.btn_1.Name = "btn_1";
+            this.btn_1.Size = new System.Drawing.Size(321, 83);
+            this.btn_1.TabIndex = 0;
+            this.btn_1.UseVisualStyleBackColor = true;
+            this.btn_1.Click += new System.EventHandler(this.btn_1_Click);
+            // 
+            // pictureBox_Left
+            // 
+            this.pictureBox_Left.Location = new System.Drawing.Point(39, 39);
+            this.pictureBox_Left.Margin = new System.Windows.Forms.Padding(30);
+            this.pictureBox_Left.Name = "pictureBox_Left";
+            this.pictureBox_Left.Size = new System.Drawing.Size(400, 350);
+            this.pictureBox_Left.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage;
+            this.pictureBox_Left.TabIndex = 1;
+            this.pictureBox_Left.TabStop = false;
+            // 
+            // pictureBox_Right
+            // 
+            this.pictureBox_Right.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.pictureBox_Right.Location = new System.Drawing.Point(985, 39);
+            this.pictureBox_Right.Margin = new System.Windows.Forms.Padding(30);
+            this.pictureBox_Right.Name = "pictureBox_Right";
+            this.pictureBox_Right.Size = new System.Drawing.Size(400, 350);
+            this.pictureBox_Right.TabIndex = 2;
+            this.pictureBox_Right.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1424, 771);
+            this.Controls.Add(this.pictureBox_Right);
+            this.Controls.Add(this.pictureBox_Left);
+            this.Controls.Add(this.groupBox_Bottom);
+            this.Name = "Form1";
+            this.Text = "RWArtMonth28 - Luhman16";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyUp);
+            this.groupBox_Bottom.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox_Left)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox_Right)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox_Bottom;
+        private System.Windows.Forms.Button btn_4;
+        private System.Windows.Forms.Button btn_3;
+        private System.Windows.Forms.Button btn_2;
+        private System.Windows.Forms.Button btn_1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.PictureBox pictureBox_Left;
+        private System.Windows.Forms.PictureBox pictureBox_Right;
+    }
+}
+
---
+++ RWArtMonth28/RWArtMonth28/Form1.cs
@@ -0,0 +1,310 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using RWArtMonth28.Properties;
+using System.Xml;
+
+namespace RWArtMonth28
+{
+    public partial class Form1 : Form
+    {
+        public enum Character
+        {
+            None,
+            Enot,
+            Survivor,
+            Monk,
+            ArtiHunter,
+            Gourmand,
+            Scavenger,
+            Rivulet,
+            Spearmaster,
+            Saint
+        }
+
+        public XmlDocument mScenarios;
+        XmlElement mCurrentElement;
+        XmlNode mStoryNode;
+        bool mEnableNext;
+        int mCurrentPage;
+        int mStory;
+        bool mDie;
+        Character mCurrentLeft;
+        Character mCurrentRight;
+
+
+        public Form1()
+        {
+            InitializeComponent();
+            mScenarios = new XmlDocument();
+            mScenarios.Load(@".\Resources\Scenarios.xml");
+            mCurrentElement = mScenarios.DocumentElement;
+            mStoryNode = mCurrentElement.FirstChild;
+
+            mEnableNext = true;
+            mCurrentPage = 2;
+            mStory = 0;
+            mDie = false;
+
+            mCurrentLeft = Character.Enot;
+            mCurrentRight = Character.None;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            SetScenario(Character.None, Character.None, "Welcome to my silly little game :3 \n You must be SILLY to be able to play. GET OUT IF YOU ARE NOT SILLY!!!!11!!! \n anYWAYYY, press any arrow keys to go to the next page" +
+                "\nThis game was made by Luhman16 on tumblr, feel free to contact me if you have any questions, requests, want the full game code, or if you just feel silly");
+        }
+
+        private void btn_1_Click(object sender, EventArgs e)
+        {
+            mStory = 0;
+            int Childs = mStoryNode.ChildNodes.Count;
+            mStoryNode = mStoryNode.ChildNodes[Childs - 4 + mStory];
+            string Page = mStoryNode.FirstChild.InnerText;
+            mCurrentLeft = GetCharacter(true);
+            mCurrentRight = GetCharacter(false);
+
+            mEnableNext = true;
+
+            Form1_KeyUp(null, null);
+        }
+
+        private void btn_2_Click(object sender, EventArgs e)
+        {
+            mStory = 1;
+
+            int Childs = mStoryNode.ChildNodes.Count;
+            mStoryNode = mStoryNode.ChildNodes[Childs - 4 + mStory];
+            string Page = mStoryNode.FirstChild.InnerText;
+            System.Console.WriteLine(Page);
+            mCurrentLeft = GetCharacter(true);
+            mCurrentRight = GetCharacter(false);
+
+            mEnableNext = true;
+
+            Form1_KeyUp(null, null);
+        }
+
+        private void btn_3_Click(object sender, EventArgs e)
+        {
+            mStory = 2;
+
+            int Childs = mStoryNode.ChildNodes.Count;
+            mStoryNode = mStoryNode.ChildNodes[Childs - 4 + mStory];
+            string Page = mStoryNode.FirstChild.InnerText;
+            System.Console.WriteLine(Page);
+            mCurrentLeft = GetCharacter(true);
+            mCurrentRight = GetCharacter(false);
+
+            mEnableNext = true;
+
+            Form1_KeyUp(null, null);
+        }
+
+        private void btn_4_Click(object sender, EventArgs e)
+        {
+            mStory = 3;
+
+            int Childs = mStoryNode.ChildNodes.Count;
+            mStoryNode = mStoryNode.ChildNodes[Childs - 4 + mStory];
+            string Page = mStoryNode.FirstChild.InnerText;
+            System.Console.WriteLine(Page);
+            mCurrentLeft = GetCharacter(true);
+            mCurrentRight = GetCharacter(false);
+
+            mEnableNext = true;
+
+            Form1_KeyUp(null, null);
+        }
+
+        public void SetScenario(Character CharaLeft, Character CharaRight, string Text)
+        {
+            SetPictureBox(pictureBox_Left, CharaLeft);
+            SetPictureBox(pictureBox_Right, CharaRight);
+
+            CheckSpecial(Text);
+
+            label1.Text = Text;
+
+            btn_1.Enabled = false;
+            btn_2.Enabled = false;
+            btn_3.Enabled = false;
+            btn_4.Enabled = false;
+            btn_1.Text = "";
+            btn_2.Text = "";
+            btn_3.Text = "";
+            btn_4.Text = "";
+
+        }
+
+        public void SetScenario(Character CharaLeft, Character CharaRight, string Text, string Button1, string Button2, string Button3, string Button4)
+        {
+            SetPictureBox(pictureBox_Left, CharaLeft);
+            SetPictureBox(pictureBox_Right, CharaRight);
+
+            CheckSpecial(Text);
+
+            label1.Text = Text;
+
+            btn_1.Enabled = true;
+            btn_2.Enabled = true;
+            btn_3.Enabled = true;
+            btn_4.Enabled = true;
+            btn_1.Text = Button1;
+            btn_2.Text = Button2;
+            btn_3.Text = Button3;
+            btn_4.Text = Button4;
+
+            System.Threading.Thread.Sleep(100);
+        }
+
+        public void SetPictureBox(PictureBox PB, Character Chara)
+        {
+            switch (Chara)
+            {
+                case Character.Enot:
+                    PB.Image = RWArtMonth28.Properties.Resources.RWEnot;
+                    break;
+                case Character.ArtiHunter:
+                    PB.Image = RWArtMonth28.Properties.Resources.RWArtiHunter;
+                    break;
+                case Character.Gourmand:
+                    PB.Image = RWArtMonth28.Properties.Resources.RWGourmand;
+                    break;
+                case Character.Monk:
+                    PB.Image = RWArtMonth28.Properties.Resources.RWMonk;
+                    break;
+                case Character.Rivulet:
+                    PB.Image = RWArtMonth28.Properties.Resources.RWRivulet;
+                    break;
+                case Character.Saint:
+                    PB.Image = RWArtMonth28.Properties.Resources.RWSaint;
+                    break;
+                case Character.Scavenger:
+                    PB.Image = RWArtMonth28.Properties.Resources.RWScav;
+                    break;
+                case Character.Spearmaster:
+                    PB.Image = RWArtMonth28.Properties.Resources.RWSpearmaster;
+                    break;
+                case Character.Survivor:
+                    PB.Image = RWArtMonth28.Properties.Resources.RWSurvivor;
+                    break;
+                default:
+                    PB.Image = null;
+                    break;
+            }
+        }
+
+        private void Form1_KeyUp(object sender, KeyEventArgs e)
+        {
+
+            if (mDie)
+            {
+                //Sound GG gamer
+                this.Close();
+            }
+            else
+            {
+                if (mEnableNext)
+                {
+                    bool HasButtons = mStoryNode.LastChild.Name.Equals("Story4");
+                    int RemoveLast = 9;
+                    if (!HasButtons)
+                    {
+                        RemoveLast = 1;
+                    }
+
+
+                    if (mCurrentPage < mStoryNode.ChildNodes.Count - RemoveLast)
+                    {
+                        string Page = mStoryNode.ChildNodes[mCurrentPage].InnerText;
+                        System.Console.WriteLine(Page);
+                        SetScenario(mCurrentLeft, mCurrentRight, Page);
+
+                        mCurrentPage++;
+                    } else
+                    {
+                        if (mCurrentPage == mStoryNode.ChildNodes.Count - RemoveLast)
+                        {
+                            string Page = mStoryNode.ChildNodes[mCurrentPage].InnerText;
+                            if (HasButtons)
+                            {
+                                string Button1 = mStoryNode.ChildNodes[mCurrentPage + 1].InnerText;
+                                string Button2 = mStoryNode.ChildNodes[mCurrentPage + 2].InnerText;
+                                string Button3 = mStoryNode.ChildNodes[mCurrentPage + 3].InnerText;
+                                string Button4 = mStoryNode.ChildNodes[mCurrentPage + 4].InnerText;
+                                System.Console.WriteLine(Page);
+                                SetScenario(mCurrentLeft, mCurrentRight, Page, Button1, Button2, Button3, Button4);
+                            }
+                            else
+                            {
+                                SetScenario(mCurrentLeft, mCurrentRight, Page);
+                                mDie = true;
+                            }
+
+                            mCurrentPage = 2;
+                            mEnableNext = false;
+                        }
+                    }
+
+                }
+
+            }
+        }
+
+        public void Backflip(bool left)
+        {
+            PictureBox Pb = pictureBox_Right;
+            if (left)
+            {
+                Pb = pictureBox_Left;
+            }
+
+            Pb.Image.RotateFlip(RotateFlipType.Rotate180FlipNone);
+            this.Update();
+            System.Threading.Thread.Sleep(100);
+        }
+
+        public Character GetCharacter(bool left)
+        {
+            Character Chara = Character.None;
+            int Index = 1;
+            if (left)
+            {
+                Index = 0;
+            }
+
+            string CharaString = mStoryNode.ChildNodes[Index].InnerText;
+            Enum.TryParse(CharaString, out Chara);
+
+            return Chara;
+        }
+
+        public void CheckSpecial(string line)
+        {
+            if (line.Contains("/Backflip"))
+            {
+                if (line.Contains("Rivulet"))
+                {
+                    Backflip(false);
+                }
+                else if (line.Contains("Enot"))
+                {
+                    Backflip(true);
+                }
+            }
+            else if (line.Contains("/Chad"))
+            {
+                pictureBox_Left.Image = RWArtMonth28.Properties.Resources.RWEnotVirgin;
+                pictureBox_Right.Image = RWArtMonth28.Properties.Resources.RWArtihunterChad;
+            }
+        }
+    }
+}
